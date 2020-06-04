06b6c3a764862ebdc6131fce09b550eeabe4c75f
Tyler Coffman <tyler.coffman@appfolio.com>
2020-06-04 14:52:22 -0700

    Remove dead code

--- android/src/main/cpp/RNCWebViewLifecycle.h
+++ android/src/main/cpp/RNCWebViewLifecycle.h
@@ -3,26 +3,10 @@
 #include <jni.h>
 #include <jsi/jsi.h>
 
+// TODO: remove this and use specific types from namespace in cpp file
 using namespace facebook;
 
 extern "C" {
-  JNIEXPORT void JNICALL
-  Java_com_reactnativecommunity_webview_jsi_Lifecycle_installBinding(JNIEnv* env, jobject thiz, jlong runtimePtr);
-
   JNIEXPORT jboolean JNICALL
   Java_com_reactnativecommunity_webview_jsi_Lifecycle_onShouldStartLoadWithRequest(JNIEnv* env, jobject thiz, jlong runtimePtr, jint viewId, jstring url);
 }
-
-class SampleModule : public jsi::HostObject {
-public:
-    static void install(
-        jsi::Runtime &runtime,
-        const std::shared_ptr<SampleModule> sampleModule
-    );
-
-//    SampleModule(JNIEnv *jniEnv);
-
-    jsi::Value get(jsi::Runtime &runtime, const jsi::PropNameID &name) override;
-private:
-    JNIEnv jniEnv_;
-};
