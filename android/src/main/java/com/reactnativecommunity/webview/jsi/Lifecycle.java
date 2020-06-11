7dd00e2b9ecbb6555145bba268e771bfdc6775f6
Daniel Vicory <dvicory@gmail.com>
2020-06-11 13:38:30 -0400

    Remove unused methods (squash)

--- android/src/main/java/com/reactnativecommunity/webview/jsi/Lifecycle.java
+++ android/src/main/java/com/reactnativecommunity/webview/jsi/Lifecycle.java
@@ -1,16 +1,9 @@
 package com.reactnativecommunity.webview.jsi;
 
 public class Lifecycle {
-  public native void installBinding(long javaScriptContextHolder);
   public native boolean onShouldStartLoadWithRequest(long javaScriptContextHolder, int viewId, String url);
-
-  private String stringField = "Private field value";
 
   static {
     System.loadLibrary("reactnativecommunity-webview");
-  }
-
-  public static String runTest() {
-    return "Static field value";
   }
 }
