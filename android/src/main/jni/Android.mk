606fd523db2a3168813b6915a06092a563e3eece
Tyler Coffman <tyler.coffman@appfolio.com>
2020-06-25 10:48:05 -0700

    Remove import-module stuff and try different approach

--- android/src/main/jni/Android.mk
+++ android/src/main/jni/Android.mk
@@ -21,14 +21,6 @@ LOCAL_C_INCLUDES := $(REACT_NATIVE)/ReactCommon/jsi $(LOCAL_PATH)/../cpp
 # LOCAL_SRC_FILES += $(REACT_NATIVE)/ReactCommon/jsi/jsi/jsi.cpp
 LOCAL_SRC_FILES += $(wildcard $(LOCAL_PATH)/../cpp/*.cpp)
 
-LOCAL_SHARED_LIBRARIES := jsireact
-
 LOCAL_MODULE := reactnativecommunity-webview
 
 include $(BUILD_SHARED_LIBRARY)
-
-$(call import-module,jsiexecutor)
-$(call import-module,jsi)
-# $(call import-module,folly_json)
-# $(call import-module,glog)
-$(call import-module,reactnative)
--- android/src/main/jni/Application.mk
+++ android/src/main/jni/Application.mk
@@ -1,4 +1,2 @@
 APP_ABI := all
 APP_STL := c++_shared
-
-NDK_MODULE_PATH := /Users/tyler.coffman/src/apm-mobile/packages/apm-mobile/node_modules/react-native/ReactCommon:
