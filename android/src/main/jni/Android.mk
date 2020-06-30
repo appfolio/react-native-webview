c0bff7f35693d4a6a9d2a81184e13a3bbddcc458
Tyler Coffman <tyler.coffman@appfolio.com>
2020-06-30 12:00:45 -0700

    Link against jscexecutor manually extracted from AAR

--- android/src/main/jni/Android.mk
+++ android/src/main/jni/Android.mk
@@ -12,7 +12,15 @@ LOCAL_CPPFLAGS += -Wall
 LOCAL_CPPFLAGS += -Wextra
 LOCAL_CPPFLAGS += -Werror
 
+LOCAL_DISABLE_FATAL_LINKER_WARNINGS := true
 LOCAL_LDLIBS := -llog
+
+LOCAL_LDLIBS += /Users/tyler.coffman/Desktop/react-native-0.62.2/jni/x86
+LOCAL_LDLIBS += /Users/tyler.coffman/Desktop/react-native-0.62.2/jni/x86_64
+LOCAL_LDLIBS += /Users/tyler.coffman/Desktop/react-native-0.62.2/jni/armeabi-v7a
+LOCAL_LDLIBS += /Users/tyler.coffman/Desktop/react-native-0.62.2/jni/arm64-v8a
+
+LOCAL_LDLIBS += -ljscexecutor
 
 LOCAL_C_INCLUDES := $(REACT_NATIVE)/ReactCommon/jsi $(LOCAL_PATH)/../cpp
 
